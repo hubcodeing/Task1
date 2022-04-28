ee13bdca07aa1bd5b6de7e16cd1bd9e362bc5f19
hubcodeing <genius.igenerate@gmail.com>
2022-04-28 19:03:51 +0530

    task1

--- task/controller/notes.js
+++ task/controller/notes.js
@@ -5,15 +5,15 @@ const Notes = require("../models/notes")
 exports.user = async (req, res) => {
     const notes = await new Notes(req.body);
     notes.save(() => {
-            try {
-                res.json({ success: true, message: " data post  successfully", data: notes })
-            } catch (err) {
-                console.log(err)
-            }
-        
+        try {
+            res.json({ success: true, message: " data post  successfully", data: notes })
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 }
-exports.get = async function (req, res){
+exports.get = async function (req, res) {
     const notes = await Notes.findOne({})
     try {
         res.json({ success: true, message: "data get", data: notes })
@@ -23,21 +23,24 @@ exports.get = async function (req, res){
 
 }
 
-exports.update = async function (req, res){
-    const notes = await Notes.findByIdAndUpdate({ _id: req.params.id }, req.body)
-    notes.save(() => {
-        try {
-            res.json({ success: true, message: "data update successfully ", data: notes })
-        } catch (err) {
-            console.log(err)
-        }
-    })
+exports.update = async function (req, res) {
+    try {
+
+        const notes = await Notes.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        const data = await notes.save();
+        res.json({ success: true, message: "data update successfully ", data })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+
+    }
+
 }
 
 exports.note = async function (req, res) {
-    const notes = await Notes.findByIdAndDelete(req.params.id)
+    const notes = await Notes.findByIdAndDelete({ _id: req.params.id })
+
     try {
-        res.json({ success: true, message: " data delete successfully" ,data:notes})
+        res.json({ success: true, message: " data delete successfully", data: notes })
     } catch (err) {
         console.log(err)
     }
--- task/controller/user.js
+++ task/controller/user.js
@@ -1,31 +1,32 @@
-const Login =require("../models/user")
-const jwt =require("jsonwebtoken")
-var secret = "login"
-exports.login=async(req,res)=>{
-    let user = await new Login();
-    user.name = req.body.name
-    user.email = req.body.email
-    user.password = req.body.password
-    user.phone = req.body.phone
-    user.save(() => {
-        try{
-            res.json({success: true ,message:"register successfully" ,data:user})
-        }catch(err){
-            console.log(err)
-        }
-    // res.send(user)
-});
-} 
- 
-exports.register= async(req,res)=>{
-    try{
-        const user = await Login.findOne({ email: req.body.email })
-        if(!user){
-        return  res.json({success:false, message:"user is already login"})
-    }else
-        var token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: '1h' });
-        res.json({ success: true ,token:token})
-    } catch(err){
-        console.log(err)
-    }
-}
+const Login = require("../models/user");
+const jwt = require("jsonwebtoken");
+var secret = "login";
+exports.login = async (req, res) => {
+  try {
+    let user = new Login();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.phone = req.body.phone;
+    await user.save();
+    res.json({ success: true, message: "register successfully", data: user });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: "register successfully" });
+
+  }
+};
+exports.register = async (req, res) => {
+  try {
+    const user = await Login.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({ success: false, message: "user is already login" });
+    } else
+      var token = jwt.sign({ email: user.email, id: user._id }, secret, {
+        expiresIn: "1h",
+      });
+    res.json({ success: true, token: token });
+  } catch (err) {
+    console.log(err);
+  }
+};
