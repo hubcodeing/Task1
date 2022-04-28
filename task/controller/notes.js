b57f69072a837632e11e592b92df77e9a0f728dc
hubcodeing <genius.igenerate@gmail.com>
2022-04-28 17:46:05 +0530

    task1

--- task/controller/notes.js
+++ task/controller/notes.js
@@ -5,8 +5,6 @@ const Notes = require("../models/notes")
 exports.user = async (req, res) => {
     const notes = await new Notes(req.body);
     notes.save(() => {
-        
-
             try {
                 res.json({ success: true, message: " data post  successfully", data: notes })
             } catch (err) {
@@ -15,7 +13,7 @@ exports.user = async (req, res) => {
         
     })
 }
-exports.get = async (req, res) => {
+exports.get = async function (req, res){
     const notes = await Notes.findOne({})
     try {
         res.json({ success: true, message: "data get", data: notes })
@@ -25,7 +23,7 @@ exports.get = async (req, res) => {
 
 }
 
-exports.update = async (req, res) => {
+exports.update = async function (req, res){
     const notes = await Notes.findByIdAndUpdate({ _id: req.params.id }, req.body)
     notes.save(() => {
         try {
@@ -36,7 +34,7 @@ exports.update = async (req, res) => {
     })
 }
 
-exports.note = async(req, res) => {
+exports.note = async function (req, res) {
     const notes = await Notes.findByIdAndDelete(req.params.id)
     try {
         res.json({ success: true, message: " data delete successfully" ,data:notes})
--- task/routes/api.js
+++ task/routes/api.js
@@ -1,39 +1,10 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-const Login = require("../models/user")
 const router = express.Router();
-secret = "task"
-var jwt =require('jsonwebtoken')
-
-router.post("/register", async (req, res) => {
-    console.log("hello",user)
-    let user = await new Login();
-    user.name = req.body.name
-    user.email = req.body.email
-    user.password = req.body.password
-    user.phone = req.body.phone
-    user.save((err) => {
-        if (err) {
-            console.log("errr")
-        } else {
-            console.log("success")
-        }
-    })
-    res.send(user)
-});
-
-router.post("/login", function (req, res) {
-    Login.findOne({ email: req.body.email }).exec(function (err, user) {
-        if (err) throw err
-        else {
-            if (!user) {
-                res.json({ success: false })
-            }
-            var token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: '1h' });
-            res.json({ success: true ,token:token})
-        }
-    })
-})
+const {login,register}=require("../controller/user");
+router.post("/register",login)
+ 
+router.post("/login",register)
 
 module.exports = router;
