067837adb8647c78cd36cf9c55c0a44839179d77
hubcodeing <genius.igenerate@gmail.com>
2022-04-29 10:47:47 +0530

    task

--- task/controller/notes.js
+++ task/controller/notes.js
@@ -1,46 +1,47 @@
-const Notes = require("../models/notes")
-
-
+const Notes = require("../models/notes");
 
 exports.user = async (req, res) => {
+  try {
     const notes = await new Notes(req.body);
-    await  notes.save(() => {
-        try {
-            res.json({ success: true, message: " data post  successfully", data: notes })
-        } catch (err) {
-            console.log(err)
-        }
-
-    })
-}
+    const data = await notes.save();
+    res.json({ success: true, message: " data post  successfully", data });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 exports.get = async function (req, res) {
-    const notes = await Notes.findOne({})
-    try {
-        res.json({ success: true, message: "data get", data: notes })
-    } catch (err) {
-        console.log(err)
-    }
-
-}
+  try {
+    const notes = await Notes.findOne({});
+    res.json({ success: true, message: "data get", data: notes });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: err.message });
+  }
+};
 
 exports.update = async function (req, res) {
-    try {
-        const notes = await Notes.findByIdAndUpdate({ _id: req.params.id }, req.body)
-        const data = await notes.save();
-        res.json({ success: true, message: "data update successfully ", data })
-    } catch (error) {
-        res.json({ success: false, message: error.message })
-
-    }
-
-}
+  try {
+    const notes = await Notes.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body
+    );
+    const data = await notes.save();
+    res.json({ success: true, message: "data update successfully ", data });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 exports.note = async function (req, res) {
-    const notes = await Notes.findByIdAndDelete({ _id: req.params.id })
+  try {
+    const notes = await Notes.findByIdAndDelete({ _id: req.params.id });
 
-    try {
-        res.json({ success: true, message: " data delete successfully", data: notes })
-    } catch (err) {
-        console.log(err)
-    }
-}
+    res.json({
+      success: true,
+      message: " data delete successfully",
+      data: notes,
+    });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
+  }
+};
